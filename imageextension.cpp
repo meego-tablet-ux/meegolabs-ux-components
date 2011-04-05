554a6ca0b9a8cb1878323d0b0f1bef65668d1ed1
Geoff Gustafson <geoff@linux.intel.com>
2011-04-05 14:57:58 -0700

    [ImageExtension] store rotations in QSettings instead of modifying image
    Signed-off-by: Geoff Gustafson <geoff@linux.intel.com>

--- imageextension.cpp
+++ imageextension.cpp
@@ -6,12 +6,22 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
+#include <QSettings>
+
 #include "imageextension.h"
-#include <QFile>
+
+const char Organization[] = "MeeGo";
+const char Application[] = "meegolabs-ux-components";
+const char Orientations[] = "orientations";
+
 ImageExtension::ImageExtension(QObject *parent) :
         QObject(parent) ,
         m_source(QString()),
-        m_ed(0)
+        m_ed(NULL),
+        m_fileOrientation(TopUp),
+        m_orientation(TopUp),
+        m_userOrientation(TopUp),
+        m_fileMirrored(false)
 {
 }
 
@@ -19,45 +29,99 @@ ImageExtension::~ImageExtension()
 {
     delete m_ed;
 }
+
+QString ImageExtension::source() const {
+    return m_source;
+}
+
 void ImageExtension::setSource(const QString source)
 {
     QString fixed_source = fixSourceName(source);
-    if (m_source == fixed_source) {
+    if (m_source == fixed_source)
         return;
-    }
     m_source = fixed_source;
 
-    //if (m_ed != 0) {
     delete m_ed;
-    delete m_image;
-    //}
     m_ed = exif_data_new_from_file(m_source.toUtf8().constData());
-    m_image = new QImage(source);
-    m_usrOrientation = orientation();
-    emit orientationChanged();
-    emit hasExifChanged();
-}
 
-int ImageExtension::orientation() const
-{
-    if (!m_ed) {
-        return 1;
+    int orientation = TopUp;
+    m_fileMirrored = false;
+    if (m_ed) {
+        ExifEntry *entry;
+        ExifByteOrder byte_order;
+        byte_order = exif_data_get_byte_order(m_ed);
+
+        entry = exif_content_get_entry(m_ed->ifd[EXIF_IFD_0], EXIF_TAG_ORIENTATION);
+
+        if (entry) {
+            // the orientation here actually contains both orientation and mirror
+            //    1	top	left side
+            //    2	top	right side
+            //    3	bottom	right side
+            //    4	bottom	left side
+            //    5	left side	top
+            //    6	right side	top
+            //    7	right side	bottom
+            //    8	left side	bottom
+            short result = exif_get_short(entry->data, byte_order);
+            if (result <= 0 || result >= 8)
+                result = 1;
+
+            int mapOrientation[] = { 0, TopUp, TopUp, BottomUp, BottomUp,
+                                     LeftUp, RightUp, RightUp, LeftUp };
+            m_fileOrientation = mapOrientation[result];
+
+            int mapMirrored[] = { 0, 0, 1, 0, 1, 1, 0, 1, 0 };
+            m_fileMirrored = mapMirrored[result];
+        }
     }
-    ExifEntry *entry;
-    ExifByteOrder byte_order;
-    byte_order = exif_data_get_byte_order (m_ed);
 
-    if ((entry = exif_content_get_entry(m_ed->ifd[EXIF_IFD_0], EXIF_TAG_ORIENTATION)))
-    {
-        return exif_get_short (entry->data, byte_order);
+    // check for orientation override
+    QSettings settings(Organization, Application);
+    settings.beginGroup(Orientations);
+    orientation = settings.value(m_source, m_fileOrientation).toInt();
 
-    }
-    return 1;
+    setOrientation(orientation);
+    setUserOrientation(orientation);
+
+    emit sourceChanged();
 }
 
 bool ImageExtension::hasExif() const
 {
-    return (m_ed != 0);
+    return m_ed ? true : false;
+}
+
+int ImageExtension::orientation() const
+{
+    return m_orientation;
+}
+
+void ImageExtension::setOrientation(int orientation)
+{
+    if (orientation >= TopUp && orientation <= RightUp && m_fileOrientation != orientation) {
+        m_orientation = orientation;
+        emit orientationChanged();
+    }
+}
+
+int ImageExtension::userOrientation() const
+{
+    return m_userOrientation;
+}
+
+void ImageExtension::setUserOrientation(int userOrientation)
+{
+    userOrientation = userOrientation % 4;
+    if (m_userOrientation!= userOrientation) {
+        m_userOrientation = userOrientation;
+        emit userOrientationChanged();
+    }
+}
+
+void ImageExtension::saveInfo()
+{
+    saveIfChanged();
 }
 
 QString ImageExtension::fixSourceName(QString source) const
@@ -68,119 +132,29 @@ QString ImageExtension::fixSourceName(QString source) const
     return source;
 }
 
-void ImageExtension::saveIfChanged() const
+void ImageExtension::saveIfChanged()
 {
-    if (!m_image) {
-        // no data to save, return;
+    if (m_source.isEmpty() || m_orientation == m_userOrientation) {
+        // no image or no change
         return;
     }
-    if (usrOrientation() != orientation()) {
-        if (hasExif()) {
-            // change the corresponding EXIF tag
-            ExifEntry *entry;
-            ExifByteOrder byte_order;
-            byte_order = exif_data_get_byte_order (m_ed);
 
-            if (entry= exif_content_get_entry(m_ed->ifd[EXIF_IFD_0], EXIF_TAG_ORIENTATION))
-            {
-                exif_set_short(entry->data, byte_order, usrOrientation());
-            }else {
-                // does not exist the ORIENTATION  tag, create a new one.
-                entry = exif_entry_new();
-                entry->tag = EXIF_TAG_ORIENTATION;
-                exif_content_add_entry(m_ed->ifd[EXIF_IFD_0], entry);
-                exif_entry_initialize(entry, EXIF_TAG_ORIENTATION);
-                exif_set_short(entry->data, byte_order,usrOrientation());
-                exif_entry_unref(entry);
-            }
-            unsigned char* exif_data;
-            unsigned int exif_data_len;
-            QFile file(m_source);
-            quint64 size = file.size();
-            quint64 startPt = -1;
-            quint64 endPt = -1;
-            unsigned exif_tag_size = 0;
-
-            exif_data_save_data(m_ed, &exif_data, &exif_data_len);
-
-            if (!file.open(QIODevice::ReadOnly)) {
-                return;
-            }
-            uchar * content = file.map(0, size);
-            if (content == 0) {
-                return;
-            }
-
-            for (startPt = 2; startPt < size; startPt += 2) {
-                if (content[startPt] == 0xff && content[startPt +1] == 0xe1) {
-                    // found the start of EXIF tag
-                    startPt += 2;
-                    break;
-                }
-            }
-            // if didn't find the exif tag
-            // should not happen
-            if (startPt == -1) {
-                return;
-            }
-
-            // if the size is incorrect ...
-            if (size < startPt + 2) {
-                return;
-            }
-             exif_tag_size = (content[startPt] << 8) + content[startPt +1] ;
-             endPt = startPt + exif_tag_size;
-            // open a temp file
-            QFile tempImage("/tmp/image.tmp");
-            if (!tempImage.open(QIODevice::WriteOnly)) {
-                file.unmap(content);
-                file.close();
-                return;
-            }
-
-            QDataStream out(&tempImage);
-            out.writeRawData((const char*)content, startPt);
-            tempImage.putChar((exif_data_len + 2) >> 8);
-            tempImage.putChar((exif_data_len + 2) & 0xff);
-            out.writeRawData((const char*)exif_data, exif_data_len);
-            out.writeRawData((const char*)(content + endPt), size - endPt);
-            tempImage.close();
-            file.unmap(content);
-            file.close();
-            QFile::remove(source());
-            QFile::copy("/tmp/image.tmp", source());
-        }else {
-            // does not have exif tag;
-            QImage image(source());
-            if (!image.isNull()) {
-                QTransform t;
-                switch (m_usrOrientation) {
-                case 1:
-                   return;
-                case 8:
-                   t.rotate(270);
-                   break;
-                case 3:
-                   t.rotate(180);
-                   break;
-                case 6:
-                   t.rotate(90);
-                   break;
-                default:
-                   return;
-                }
-                 image = image.transformed(t);
-                 image.save(source());
-                 return;
-            }
-        }
-    }else {
-        // no change
-        return;
+    QSettings settings(Organization, Application);
+    settings.beginGroup(Orientations);
+    if (m_userOrientation == m_fileOrientation)
+        // remove override
+        settings.remove(m_source);
+    else {
+        // override orientation in settings
+        settings.setValue(m_source, m_userOrientation);
+        settings.sync();
     }
 }
 
-void ImageExtension::saveInfo()
+int ImageExtension::exifOrientation() const
 {
-    saveIfChanged();
+    // requires: m_fileOrientation is valid, between 0 and 3
+    int mapOrientation[] = { 1, 8, 3, 6, 2, 5, 4, 7 };
+    int index = m_userOrientation + (m_fileMirrored ? 4 : 0);
+    return mapOrientation[index];
 }
--- imageextension.h
+++ imageextension.h
@@ -6,74 +6,76 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
-#ifndef IMAGEEXTENTION_H
-#define IMAGEEXTENTION_H
+#ifndef IMAGEEXTENSION_H
+#define IMAGEEXTENSION_H
 #include <QDeclarativeParserStatus>
 #include <QObject>
 #include <QImage>
 #include <libexif/exif-data.h>
-class ImageExtension : public QObject, public QDeclarativeParserStatus
+
+class ImageExtension: public QObject, public QDeclarativeParserStatus
 {
     Q_OBJECT
+
 public:
     explicit ImageExtension(QObject *parent = 0);
-
     ~ImageExtension();
 
     Q_INTERFACES(QDeclarativeParserStatus);
 
-    Q_PROPERTY(QString source READ source WRITE setSource);
+    Q_PROPERTY(QString source READ source WRITE setSource NOTIFY sourceChanged);
+    Q_PROPERTY(bool hasExif READ hasExif);
     Q_PROPERTY(int orientation READ orientation NOTIFY orientationChanged);
-    Q_PROPERTY(bool hasExif  READ hasExif NOTIFY hasExifChanged);
-    Q_PROPERTY(int usrOrientation READ usrOrientation WRITE setUsrOrientation);
+    Q_PROPERTY(int userOrientation READ userOrientation WRITE setUserOrientation NOTIFY userOrientationChanged);
+
+    enum Orientation {
+        TopUp,
+        LeftUp,
+        BottomUp,
+        RightUp
+    };
+    Q_ENUMS(Orientation)
 
 public:
-    QString source() const {
-        return m_source;
-    }
+    QString source() const;
     void setSource(const QString source);
-
-    // the orientation here actually contains both orientation and mirror
-    //    1	top	left side
-    //    2	top	right side
-    //    3	bottom	right side
-    //    4	bottom	left side
-    //    5	left side	top
-    //    6	right side	top
-    //    7	right side	bottom
-    //    8	left side	bottom
-    int orientation() const;
-
-    int usrOrientation() const
-    {
-        return m_usrOrientation;
-    }
-    void setUsrOrientation(const int usrOrientation)
-    {
-        if (usrOrientation > 0 && usrOrientation < 9 ){
-            m_usrOrientation = usrOrientation;
-        }
-    }
 
     bool hasExif() const;
 
-    virtual void classBegin() {}
+    // the orientation specified by the underlying file or overridden value from settings
+    int orientation() const;
 
-    virtual void componentComplete(){}
+    // the orientation the user has set through rotation
+    int userOrientation() const;
+    void setUserOrientation(int userOrientation);
+
+    // TODO: apparently some optimization was intended here
+    virtual void classBegin() {}
+    virtual void componentComplete() {}
 
 public slots:
     void saveInfo();
+
 signals:
+    void sourceChanged();
     void orientationChanged();
-    void hasExifChanged();
+    void userOrientationChanged();
+    void mirroredChanged();
+
 protected:
+    void setOrientation(int orientation);
     QString fixSourceName(const QString source) const;
-    void saveIfChanged() const;
+    void saveIfChanged();
+    int exifOrientation() const;
+
 private:
     QString m_source;
-    ExifData * m_ed;
-    int m_usrOrientation;
-    QImage *m_image;
+    ExifData *m_ed;
+
+    int m_fileOrientation;
+    int m_orientation;
+    int m_userOrientation;
+    bool m_fileMirrored;
 };
 
-#endif // IMAGEEXTENTION_H
+#endif // IMAGEEXTENSION_H
